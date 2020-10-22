011e74bb977e97285c25e1b5e61a17ad77637747
OlgaKor23 <work.olga.koryshkina@gmail.com>
2020-10-22 21:39:55 +0300

    reuploaded module 7

---
+++ task_07.js
@@ -0,0 +1,1 @@
+const inputRef = document.querySelector('#font-size-control');const spanRef = document.querySelector('#text');inputRef.addEventListener('input', onInputChange);function onInputChange(event) {    const box = event.target.value;    spanRef.style.fontSize = box + 'px';}
